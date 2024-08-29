9d5170e92da156ed8e5425e0de7142f70e8916ee
Minji Kim <minjikim2000@hotmail.com>
2024-08-30 01:41:35 +0900

    Swiper #1
    홍보 배너로 활용

--- Movies/screens/Movies.tsx
+++ Movies/screens/Movies.tsx
@@ -1,26 +1,32 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React from 'react';
+import { Dimensions } from 'react-native'; // 화면의 사이즈 가져다 줌
+import Swiper from 'react-native-web-swiper';
 import styled from 'styled-components/native';
 
-const Btn = styled.TouchableOpacity`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+const ScrollView = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
 `;
 
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
+const View = styled.View`
+  flex: 1;
 `;
 
-const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({
-  navigation: { navigate },
-}) => (
-  <Btn
-    onPress={() => navigate('Stack', { screen: 'Three' })}
-    style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-  >
-    <Title>Movies</Title>
-  </Btn>
+//동일하지만 아래 형식을 더 선호 => const SCREEN_HEIGHT = Dimensions.get("window").height;
+const { height: SCREEN_HEIGHT } = Dimensions.get('window');
+
+const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = () => (
+  <ScrollView>
+    <Swiper
+      loop
+      timeout={2}
+      controlsEnabled={false}
+      containerStyle={{ width: '100%', height: SCREEN_HEIGHT / 4 }}
+    >
+      <View style={{ backgroundColor: 'red' }}></View>
+      <View style={{ backgroundColor: 'blue' }}></View>
+      <View style={{ backgroundColor: 'green' }}></View>
+    </Swiper>
+  </ScrollView>
 );
 export default Movies;
