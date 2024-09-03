16f872b4f50ac4cbe3d97be5b08e8233c60c6361
Minji Kim <minjikim2000@hotmail.com>
2024-09-03 19:51:29 +0900

    navigation params, setOptions #1

--- Movies/screens/Detail.tsx
+++ Movies/screens/Detail.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
+import styled from 'styled-components/native';
 
-const Detail = () => {
+const Detail = ({
+  navigation: { setOptions },
+  route: {
+    params: { originalTitle },
+  },
+}) => {
+  useEffect(() => {
+    setOptions({ title: originalTitle });
+  }, []);
+  console.log(originalTitle);
   return (
-    <View>
-      <Text>Detail</Text>
-    </View>
+    <Container>
+      <Text>{originalTitle}</Text>
+    </Container>
   );
 };
+
+const Container = styled.ScrollView`
+  background-color: ${(props) => props.theme.mainBgColor};
+`;
 
 export default Detail;
