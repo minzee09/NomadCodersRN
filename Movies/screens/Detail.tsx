5f4cb497ab0d3e51d6411bf2a78c69f30fb33e63
Minji Kim <minjikim2000@hotmail.com>
2024-09-03 19:57:16 +0900

    refactor: add typescript types

--- Movies/screens/Detail.tsx
+++ Movies/screens/Detail.tsx
@@ -1,8 +1,14 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import styled from 'styled-components/native';
 
-const Detail = ({
+type RootStackParamList = {
+  Detail: { originalTitle: string };
+};
+type DetailScreenProps = NativeStackScreenProps<RootStackParamList, 'Detail'>;
+
+const Detail: React.FC<DetailScreenProps> = ({
   navigation: { setOptions },
   route: {
     params: { originalTitle },
