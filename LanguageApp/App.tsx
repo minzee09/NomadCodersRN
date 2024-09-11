4741948b586447b04ddfd4ed852ddc7505ee7735
Minji Kim <minjikim2000@hotmail.com>
2024-09-11 19:44:28 +0900

    useRef, Animated #1

--- LanguageApp/App.tsx
+++ LanguageApp/App.tsx
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from "react";
+import { Animated, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
 
-export default function App() {
+const App: React.FC = () => {
+  const Y = useRef(new Animated.Value(200)).current; // useRef: 다시 렌더링이 일어나더라도 value를 유지하게 해줌, rerender하더라도 값이 초기값으로 돌아가지 않음.
+  const [up, setUp] = useState(false);
+  const toggleUp = () => setUp((prev) => !prev);
+
+  const moveUp = () => {
+    Animated.timing(Y, {
+      toValue: up ? 200 : -200,
+      useNativeDriver: true, // 항상 true -> 관련 데이터들이 native쪽으로 전달
+    }).start(toggleUp);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <TouchableOpacity onPress={moveUp}>
+        <AnimatedBox style={{ transform: [{ translateY: Y }] }} />
+      </TouchableOpacity>
+    </Container>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Box = styled.View`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
+const AnimatedBox = Animated.createAnimatedComponent(Box);
