c803c95b3e6c31283f19901f0e22cfc325b35ee1
Minji Kim <minjikim2000@hotmail.com>
2024-09-11 20:41:15 +0900

    Animated sequence & loop #1

--- LanguageApp/App.tsx
+++ LanguageApp/App.tsx
@@ -1,18 +1,43 @@
 import React, { useRef, useState } from "react";
-import { Animated, TouchableOpacity } from "react-native";
+import { Animated, Dimensions, TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 
-const App: React.FC = () => {
-  const POSITION = useRef(new Animated.ValueXY({ x: 0, y: 300 })).current; // useRef: 다시 렌더링이 일어나더라도 value를 유지하게 해줌, rerender하더라도 값이 초기값으로 돌아가지 않음.
-  const [up, setUp] = useState(false);
-  const toggleUp = () => setUp((prev) => !prev);
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
+const App: React.FC = () => {
+  const POSITION = useRef(
+    new Animated.ValueXY({ x: -SCREEN_WIDTH / 2 + 100, y: -SCREEN_HEIGHT / 2 + 100 }),
+  ).current; // useRef: 다시 렌더링이 일어나더라도 value를 유지하게 해줌, rerender하더라도 값이 초기값으로 돌아가지 않음.
+  const topLeft = Animated.timing(POSITION, {
+    toValue: {
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    useNativeDriver: false, // bg색 에니메이션 주려면 false로 해야 적용 됨
+  });
+  const bottomLeft = Animated.timing(POSITION, {
+    toValue: {
+      x: -SCREEN_WIDTH / 2 + 100,
+      y: SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false, // bg색 에니메이션 주려면 false로 해야 적용 됨
+  });
+  const bottomRight = Animated.timing(POSITION, {
+    toValue: {
+      x: SCREEN_WIDTH / 2 - 100,
+      y: SCREEN_HEIGHT / 2 - 100,
+    },
+    useNativeDriver: false, // bg색 에니메이션 주려면 false로 해야 적용 됨
+  });
+  const topRight = Animated.timing(POSITION, {
+    toValue: {
+      x: SCREEN_WIDTH / 2 - 100,
+      y: -SCREEN_HEIGHT / 2 + 100,
+    },
+    useNativeDriver: false, // bg색 에니메이션 주려면 false로 해야 적용 됨
+  });
   const moveUp = () => {
-    Animated.timing(POSITION, {
-      duration: 3000,
-      toValue: up ? 300 : -300,
-      useNativeDriver: true, // 항상 true -> 관련 데이터들이 native쪽으로 전달
-    }).start(toggleUp);
+    Animated.loop(Animated.sequence([bottomLeft, bottomRight, topRight, topLeft])).start();
   };
   // Interpolation
   const opacityValue = POSITION.y.interpolate({
@@ -38,7 +63,9 @@ const App: React.FC = () => {
           style={{
             borderRadius: borderRadius,
             backgroundColor: bgColor,
-            transform: [{ rotateY: rotation }, { translateY: POSITION.y }],
+            transform: [
+              ...POSITION.getTranslateTransform(), // 이거랑 같음 { translateY: POSITION.y },{ translateX: POSITION.x }
+            ],
           }}
         />
       </TouchableOpacity>
