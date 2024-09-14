c160bbbf00a6858f102ca56b1f72cc6b31489132
Minji Kim <minjikim2000@hotmail.com>
2024-09-15 02:40:04 +0900

    feat: add CardSwipe project

--- LanguageApp/App.tsx
+++ LanguageApp/App.tsx
@@ -1,93 +1,98 @@
 import React, { useRef, useState } from "react";
-import { Animated, Dimensions, PanResponder } from "react-native";
+import { Animated, PanResponder } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import styled from "styled-components/native";
-
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
+import icons from "./icons";
 
 const App: React.FC = () => {
-  const POSITION = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current; // useRef: 다시 렌더링이 일어나더라도 value를 유지하게 해줌, rerender하더라도 값이 초기값으로 돌아가지 않음.
-  const topLeft = Animated.timing(POSITION, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: false, // bg색 에니메이션 주려면 false로 해야 적용 됨
+  // Values
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.Value(0)).current;
+  const rotation = position.interpolate({
+    inputRange: [-250, 250],
+    outputRange: ["-15deg", "15deg"],
+    extrapolate: "extend", // input의 범위 바깥으로 나갔을 때 어떻게 처리할 지 명시
   });
-  const bottomLeft = Animated.timing(POSITION, {
-    toValue: {
-      x: -SCREEN_WIDTH / 2 + 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: false, // bg색 에니메이션 주려면 false로 해야 적용 됨
-  });
-  const bottomRight = Animated.timing(POSITION, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: SCREEN_HEIGHT / 2 - 100,
-    },
-    useNativeDriver: false, // bg색 에니메이션 주려면 false로 해야 적용 됨
-  });
-  const topRight = Animated.timing(POSITION, {
-    toValue: {
-      x: SCREEN_WIDTH / 2 - 100,
-      y: -SCREEN_HEIGHT / 2 + 100,
-    },
-    useNativeDriver: false, // bg색 에니메이션 주려면 false로 해야 적용 됨
-  });
-  // Interpolation
-  const opacityValue = POSITION.y.interpolate({
+  const secondScale = position.interpolate({
     inputRange: [-300, 0, 300],
-    outputRange: [1, 0, 1],
+    outputRange: [1, 0.5, 1],
+    extrapolate: "clamp",
   });
-  const borderRadius = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: [100, 0],
+  // Animations
+  const onPressIn = Animated.spring(scale, { toValue: 0.95, useNativeDriver: true });
+  const onPressOut = Animated.spring(scale, { toValue: 1, useNativeDriver: true });
+  const goCenter = Animated.spring(position, { toValue: 0, useNativeDriver: true });
+  const goLeft = Animated.spring(position, {
+    toValue: -500,
+    tension: 5,
+    useNativeDriver: true,
+    restDisplacementThreshold: 100, // animation 기간 조절 (여기서는 더 짧게 전환 되도록 하였음)
+    restSpeedThreshold: 100,
   });
-  const rotation = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: ["-360deg", "360deg"],
+  const goRight = Animated.spring(position, {
+    toValue: 500,
+    tension: 5,
+    useNativeDriver: true,
+    restDisplacementThreshold: 100,
+    restSpeedThreshold: 100,
   });
-  const bgColor = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: ["rgb(255,99,71)", "rgb(71,166,255)"],
-  });
+  // Pan Responders
   const panResponder = useRef(
     PanResponder.create({
-      onStartShouldSetPanResponder: () => true, // touch 감지
-      onPanResponderGrant: () => {
-        console.log("Touch Started");
-        // 마지막으로 놓인 위치에서 시작하도록
-        POSITION.setOffset({
-          x: POSITION.x._value, // number로 지정하기 위해
-          y: POSITION.y._value,
-        });
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, { dx }) => {
+        position.setValue(dx);
       },
-      onPanResponderMove: (_, { dx, dy }) => {
-        console.log("Touching");
-        // 움직임에 대한 정보를 받을 수 있음
-        POSITION.setValue({
-          x: dx,
-          y: dy,
-        });
-      },
-      onPanResponderRelease: () => {
-        console.log("Touch Finished");
-        POSITION.flattenOffset(); // offSet을 다시 0으로 초기화
+      onPanResponderGrant: () => onPressIn.start(),
+      onPanResponderRelease: (_, { dx }) => {
+        if (dx < -250) {
+          goLeft.start(onDismiss);
+        } else if (dx > 250) {
+          goRight.start(onDismiss);
+        } else {
+          Animated.parallel([onPressOut, goCenter]).start();
+        }
       },
     }),
   ).current;
+
+  // State
+  const [index, setIndex] = useState(0);
+  const onDismiss = () => {
+    scale.setValue(1);
+    position.setValue(0);
+    setIndex((prev) => prev + 1);
+  };
+  const closePress = () => {
+    goLeft.start(onDismiss);
+  };
+  const checkPress = () => {
+    goRight.start(onDismiss);
+  };
+
   return (
     <Container>
-      <AnimatedBox
-        {...panResponder.panHandlers} // function 제공 받기 위해, panHandlers는 그냥 함수 묶음
-        style={{
-          borderRadius: borderRadius,
-          backgroundColor: bgColor,
-          transform: [
-            ...POSITION.getTranslateTransform(), // 이거랑 같음 { translateY: POSITION.y },{ translateX: POSITION.x }
-          ],
-        }}
-      />
+      <CardContainer>
+        <AnimatedCard {...panResponder.panHandlers} style={{ transform: [{ scale: secondScale }] }}>
+          <Ionicons name={icons[index + 1]} color="#192a56" size={98} />
+        </AnimatedCard>
+        <AnimatedCard
+          {...panResponder.panHandlers}
+          style={{
+            transform: [{ scale: scale }, { translateX: position }, { rotateZ: rotation }],
+          }}
+        >
+          <Ionicons name={icons[index]} color="#192a56" size={98} />
+        </AnimatedCard>
+      </CardContainer>
+      <BtnContainer>
+        <Btn onPress={closePress}>
+          <Ionicons name="close-circle" color="white" size={58} />
+        </Btn>
+        <Btn onPress={checkPress}>
+          <Ionicons name="checkmark-circle" color="white" size={58} />
+        </Btn>
+      </BtnContainer>
     </Container>
   );
 };
@@ -98,12 +103,33 @@ const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  background-color: #00a8ff;
 `;
 
-const Box = styled.View`
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
+const Card = styled.View`
+  position: absolute;
+  background-color: white;
+  width: 300px;
+  height: 300px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px;
+  elevation: 6;
 `;
 
-const AnimatedBox = Animated.createAnimatedComponent(Box);
+const AnimatedCard = Animated.createAnimatedComponent(Card);
+
+const Btn = styled.TouchableOpacity`
+  margin: 0 10px;
+`;
+
+const BtnContainer = styled.View`
+  flex-direction: row;
+  flex: 1;
+`;
+
+const CardContainer = styled.View`
+  flex: 3;
+  justify-content: center;
+  align-items: center;
+`;
